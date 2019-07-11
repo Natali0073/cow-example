3ee718ff22bd34e0937d4e745c5798a5d85f7969
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 21:51:29 +0300

    add default server route

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
 // Create the server
-const app = express()
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
     try {
