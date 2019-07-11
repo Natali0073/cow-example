cf69c4c15fd51e16e307ae8e97ba1e5cc058b327
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 23:27:46 +0300

    Remove bodyparser

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
+// const cowsay = require('cowsay')
+// const cors = require('cors')
 // Create the server
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 // Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-    // try {
-        // const text = req.params.say
-        // const moo = cowsay.say({ text })
-        res.json({ moo : "kek" })
-// } catch (err) {
-//     next(err)
-// }
-})
+// app.get('/api/cow/:say', cors(), async (req, res, next) => {
+//     // try {
+//         // const text = req.params.say
+//         // const moo = cowsay.say({ text })
+//         res.json({ moo : "kek" })
+// // } catch (err) {
+// //     next(err)
+// // }
+// })
 // Serve our base route that returns a Hello World cow
-app.get('/api/cow/', (req, res) => {
+app.get('/api/cow', (req, res) => {
     // try {
     //     const moo = cowsay.say({ text: 'Hello World!' })
-    console.log("asdasda sadasdadsa")
-        res.json({ moo : "lol" })
+    console.log("asdasda sadasdadsa");
+        res.json({ moo : "lol" });
 // } catch (err) {
 //     next(err)
 // }
-})
+});
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Mixing it up on port ${PORT}`)
-})
+app.listen(PORT);
+
+console.log(`Password generator listening on ${PORT}`);
