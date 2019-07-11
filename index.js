ac1a09b293ce1d6f4840b00acc213a720effc49b
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 22:28:13 +0300

    Added static

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 // Create the server
 const app = express();
 
+app.use(express.static(__dirname));
 app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
