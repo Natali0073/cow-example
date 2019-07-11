4cbe73a386fddead84855f2dfac76d3f32e5208d
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 22:09:26 +0300

    Lol

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const cors = require('cors')
 // Create the server
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
-}
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
     try {
