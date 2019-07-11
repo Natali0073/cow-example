94939cdba4cdbe76e0e360568b1aa0f8b8186d82
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 23:07:17 +0300

    Check cow

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ app.get('/*', function (req, res) {
 });
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
-    try {
-        const text = req.params.say
-        const moo = cowsay.say({ text })
-        res.json({ moo })
-} catch (err) {
-    next(err)
-}
+    // try {
+        // const text = req.params.say
+        // const moo = cowsay.say({ text })
+        res.json({ moo: "kek" })
+// } catch (err) {
+//     next(err)
+// }
 })
 // Serve our base route that returns a Hello World cow
 app.get('/api/cow/', cors(), async (req, res, next) => {
-    try {
-        const moo = cowsay.say({ text: 'Hello World!' })
-        res.json({ moo })
-} catch (err) {
-    next(err)
-}
+    // try {
+    //     const moo = cowsay.say({ text: 'Hello World!' })
+        res.json({ moo: "lol" })
+// } catch (err) {
+//     next(err)
+// }
 })
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
