16361171647dff1c5ffa12e3a4c5f6caead1a66e
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 20:07:15 +0300

    Add server prod check

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/*', function (req, res, next) {
-    if (!req.path.includes('api'))
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    else next();
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+}
 
 app.get('/api/posts', (req, res) => {
     res.send(postsList);
